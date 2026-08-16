2571b14b886f3b6bb9d4e21e12543155e6e5172a
JaOnwude <ojamesuchenna61@gmail.com>
2026-08-16 23:47:12 +0100

    change positioning of lib folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,7 @@
-// import "@/styles/globals.css";
-// import type { AppProps } from "next/app";
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />;
-// }
-
-
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './lib/queryClient';
+import { queryClient } from '../lib/queryClient';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
